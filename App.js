25dd60843a42b3ee6d0b5f453fb31941a8db1e55
anujawas <awasthianuj394@gmail.com>
2023-12-28 01:18:03 +0530

    Managed all the Tabs

--- App.js
+++ App.js
@@ -4,28 +4,21 @@ import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 
-import { View, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons'
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Tabs from "./src/components/Tabs";
+
 
 const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <CurrentWeather />
-        {/* <UpcomingWeather /> */}
-        {/* <City /> */}
-      </View>
+    <NavigationContainer >
+      <Tabs />
     </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 export default App;
