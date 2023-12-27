c38ee63b049960139198ae411a5d9a80b67009d3
anujawas <awasthianuj394@gmail.com>
2023-12-28 01:16:56 +0530

    Adjusted the top margin for city screen

--- src/screens/City.js
+++ src/screens/City.js
@@ -26,7 +26,6 @@ const City = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: StatusBar.currentHeight || 0
     },
     imageLayout: {
         flex: 1
