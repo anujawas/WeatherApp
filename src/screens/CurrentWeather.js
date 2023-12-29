05ce3f071f76e9dedb78961fe21ada3246f32301
anujawas <awasthianuj394@gmail.com>
2023-12-29 22:58:10 +0530

    Linked WeatherType instead hardcoded Type and msg

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -3,6 +3,8 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import RowText from '../components/RowText';
 
+import { weatherType } from '../utilities/weatherType';
+
 const CurrentWeather = () => {
     const { wrapper, container, temp, feels, highLowWrapper, highLow, bodyWrapper, description, message } = styles
 
@@ -13,9 +15,11 @@ const CurrentWeather = () => {
                 <Text style={temp}>6</Text>
                 <Text style={feels}>Feels like 5</Text>
 
-                <RowText containerStyles={highLowWrapper} msg1Style={highLow} msg2Style={highLow} msg1={"High: 8 "} msg2={"Low: 6"} />
+                <RowText containerStyles={highLowWrapper} msg1Style={highLow}
+                    msg2Style={highLow} msg1={"High: 8 "} msg2={"Low: 6"} />
             </View>
-            <RowText containerStyles={bodyWrapper} msg1Style={description} msg2Style={message} msg1={"It's Sunny"} msg2={"It's perfect T-Shirt Weather"} />
+            <RowText containerStyles={bodyWrapper} msg1Style={description}
+                msg2Style={message} msg1={"It's Sunny"} msg2={weatherType['Thunderstorm'].msg} />
         </SafeAreaView>
     );
 }
