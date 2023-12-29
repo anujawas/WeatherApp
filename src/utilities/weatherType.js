b22948d2980cfb6e23a99368e079fdb3075dae84
anujawas <awasthianuj394@gmail.com>
2023-12-29 22:56:50 +0530

    Defined weather types

---
+++ src/utilities/weatherType.js
@@ -0,0 +1,42 @@
+export const weatherType = {
+    Thunderstorm: {
+        icon: 'zap',
+        msg: 'It could get noisy',
+        bgColor: '#000000'
+    },
+    Drizzle: {
+        icon: 'cloud-rain',
+        msg: 'It might rain a little',
+        bgColor: '#36454f'
+    },
+    Rain: {
+        icon: 'umbrella',
+        msg: 'You will need an umbrella',
+        bgColor: '#0000ff'
+    },
+    Snow: {
+        icon: 'cloud-snow',
+        msg: 'Lets build a snowman',
+        bgColor: '#7f6065'
+    },
+    Clear: {
+        icon: 'sun',
+        msg: 'It is perfect t-shirt weather',
+        bgColor: '#e47200'
+    },
+    Clouds: {
+        icon: 'cloud',
+        msg: 'You could live in the clouds',
+        bgColor: '#363636'
+    },
+    Haze: {
+        icon: 'wind',
+        msg: 'It might be a bit damp',
+        bgColor: '#58586e'
+    },
+    Mist: {
+        icon: 'align-justify',
+        msg: 'It might be hard to see',
+        bgColor: '#3e3e37'
+    }
+}
